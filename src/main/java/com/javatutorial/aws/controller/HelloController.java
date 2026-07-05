5f81fa9edd062977f8c5ea89bde894f6f96540f6
Roshan Shaw <hellofi@Roshan-Shaw.local>
2026-07-05 16:53:00 +0530

    Initial Commit

---
+++ src/main/java/com/javatutorial/aws/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.javatutorial.aws.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello AWS 🚀";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is Running";
+    }
+}
