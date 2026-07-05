35a66d826fa9446d3afb899d50b82f2eb60b2b8c
Roshan Shaw <shawroshan0312@gmail.com>
2026-07-05 18:02:08 +0530

    Updated Code and  deploy fix

--- src/main/java/com/javatutorial/aws/controller/HelloController.java
+++ src/main/java/com/javatutorial/aws/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello AWS 🚀";
+        return "Hello AWS. How you doing?🚀";
     }
 
     @GetMapping("/health")
