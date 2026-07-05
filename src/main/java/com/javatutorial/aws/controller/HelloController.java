441e6d29f5ef049d99109adcb7648219ada4c3ab
Roshan Shaw <shawroshan0312@gmail.com>
2026-07-06 01:18:23 +0530

    implemented docker

--- src/main/java/com/javatutorial/aws/controller/HelloController.java
+++ src/main/java/com/javatutorial/aws/controller/HelloController.java
@@ -3,15 +3,12 @@ package com.javatutorial.aws.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String hello() {
-        return "Hello from EC2.This is running using Elastic IP.";
-    }
-
-    @GetMapping("/intro")
-    public String health() {
-        return "This is Roshan Shaw";
+        return "Hello Docker!";
     }
 }
